aca1582809ebf2cb8221892c3cf4d5fc97fe81d2
tanya-pix <86041090+tanya-pix@users.noreply.github.com>
2021-07-10 17:27:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,41 +3,10 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
-mytitle='Beer Comparison'
-tabtitle='CSR Scorecard'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
+########### 
 githublink='https://github.com/austinlasseter/flying-dog-beers'
 sourceurl='https://www.flyingdog.com/beers/'
-
 ########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
-
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 
 
 ########### Initiate the app
@@ -47,13 +16,30 @@ server = app.server
 #app.title=tabtitle
 
 ########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1(myheading),
-    dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig),
-    ]
-)
+app.layout = html.Div([
+    html.Div([
+        html.Div([
+            html.Img(src=app.get_asset_url('adp-png-logo-6428.png'),
+                     id='ADPlogo',
+                     style={'height': '90px',
+                            'width': 'auto',
+                            'margin-bottom': '25px'})
+        ],className='one-third column'),
+        html.Div([
+            html.Div([
+                html.H3('ADP India', style={'margin-bottom': '0px', 'color': 'red','font-family':'Playfair','font-size': '100px'}),
+                html.H5('Corporate Social Responsibility Scorecard 2020', style={'margin-bottom': '0px', 'color': 'blue'})
+            ])
+
+        ], className='one-halfc column', id='title'),
+
+        html.Div([
+            html.H6('Last Updated: 9th July, 2021',
+                    style={'color': 'black'})
+        ], className='one-third column', id='title1')
+        ],id='header',className='row flex-display', style ={'margin-bottom': '25px'}),
+    
+    ],id='mainContainer',style={'display':'flex','flex-direction':'column'})
 
 if __name__ == '__main__':
     app.run_server()
